4e8b7a911acb51824e249813e3cc7ada017d2a39
cagestrike <cagestrike@gmail.com>
2019-04-16 15:10:33 +0200

    get calorie count

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ let request = new XMLHttpRequest();
 const app = document.getElementById('app');
 const dropdown = document.getElementById('dropdown');
 const searchBox = document.getElementById('search-box');
+const searchScreen = document.getElementById('search-screen');
+const calorieScreen = document.getElementById('calorie-screen');
 
 searchBox.addEventListener('keyup', sendRequest);
+
+
+function setHTTPHeaders(request) {
+    request.setRequestHeader("x-app-id", "db151094");
+    request.setRequestHeader("x-app-key", "37ea7f59f00e14444a1c9a73a69ca4a3");
+    request.setRequestHeader("Content-Type", "application/json");
+}
 
 function sendRequest() {
     let input = searchBox.value;
@@ -12,8 +21,7 @@ function sendRequest() {
     dropdown.innerHTML = "";
 
     request.open('GET', 'https://trackapi.nutritionix.com/v2/search/instant?query=' + input, true);
-    request.setRequestHeader("x-app-id", "db151094");
-    request.setRequestHeader("x-app-key", "37ea7f59f00e14444a1c9a73a69ca4a3");
+    setHTTPHeaders(request);
 
     request.onload = displaySuggestions;
 
@@ -37,6 +45,8 @@ function createInstantListDOM(data, instantList) {
         instantList.append(h2);
 
         for (let i = 0; i < 5; i++) {
+            let currentFood = data[type][i];
+
             const div = document.createElement('div');
             div.setAttribute('class', 'food-item');
 
@@ -47,21 +57,63 @@ function createInstantListDOM(data, instantList) {
             imgWrapper.setAttribute('class', 'img-wrapper');
 
             const thumbnail = document.createElement('img');
-            thumbnail.setAttribute('src', data[type][i].photo.thumb);
+            thumbnail.setAttribute('src', currentFood.photo.thumb);
             imgWrapper.appendChild(thumbnail);
             div.appendChild(imgWrapper);
 
             const foodName = document.createElement('p');
-            foodName.textContent = data[type][i].food_name;
+            foodName.textContent = currentFood.food_name;
 
             div.append(foodName);
 
             instantList.appendChild(li);
 
+            if (type == 'common') {
+                div.addEventListener('click', function () { getCommonNutrients(currentFood.food_name) });
+            } else if (type == 'branded') {
+                div.addEventListener('click', function () { getBrandedNutrients(currentFood.nix_item_id) });
+            }
         }
 
         dropdown.append(instantList);
     }
 }
 
+function getCommonNutrients(foodName) {
+    console.log(foodName);
+
+    request.open('POST', "https://trackapi.nutritionix.com/v2/natural/nutrients");
+    setHTTPHeaders(request);
+
+    searchScreen.style.display = "none";
+    calorieScreen.style.display = 'block';
+
+    let params = {
+        "query": foodName
+    };
+
+    request.onload = function () {
+        let data = JSON.parse(this.response);
+        console.log(data);
+    }
+
+    request.send(JSON.stringify(params));
+}
+
+function getBrandedNutrients(foodID) {
+    console.log(foodID);
+
+    searchScreen.style.display = "none";
+    calorieScreen.style.display = 'block';
+
+    request.open('GET', 'https://trackapi.nutritionix.com/v2/search/item?nix_item_id=' + foodID);
+    setHTTPHeaders(request);
+
+    request.onload = function () {
+        let data = JSON.parse(this.response);
+        console.log(data);
+    }
+
+    request.send();
+}
 
