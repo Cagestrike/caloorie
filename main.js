551a7ef74ec9c8436859b23027518f04a3707521
cagestrike <cagestrike@gmail.com>
2019-06-07 10:14:11 +0200

    fully displaying nutritional value of chosen food

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const dropdown = document.getElementById('dropdown');
 const searchBox = document.getElementById('search-box');
 const searchScreen = document.getElementById('search-screen');
 const calorieScreen = document.getElementById('calorie-screen');
+const nutritionInfo = document.getElementById('nutrition-info');
 
 searchBox.addEventListener('keyup', sendRequest);
 
@@ -86,16 +87,13 @@ function getCommonNutrients(foodName) {
     setHTTPHeaders(request);
 
     searchScreen.style.display = "none";
-    calorieScreen.style.display = 'block';
+    calorieScreen.classList.add('show');
 
     let params = {
         "query": foodName
     };
 
-    request.onload = function () {
-        let data = JSON.parse(this.response);
-        console.log(data);
-    }
+    request.onload = createNutritionList;
 
     request.send(JSON.stringify(params));
 }
@@ -104,16 +102,63 @@ function getBrandedNutrients(foodID) {
     console.log(foodID);
 
     searchScreen.style.display = "none";
-    calorieScreen.style.display = 'block';
+    calorieScreen.classList.add('show');
 
     request.open('GET', 'https://trackapi.nutritionix.com/v2/search/item?nix_item_id=' + foodID);
     setHTTPHeaders(request);
 
-    request.onload = function () {
-        let data = JSON.parse(this.response);
-        console.log(data);
-    }
+    request.onload = createNutritionList;
 
     request.send();
 }
+
+function createNutritionList() {
+    let data = JSON.parse(this.response);
+    console.log(data);
+    let food = data.foods[0];
+    let h2 = calorieScreen.firstElementChild;
+    h2.innerHTML = "showing info for " + food.serving_qty + " " + food.serving_unit + "(" + food.serving_weight_grams + "g)";
+
+    let list = document.createDocumentFragment();
+
+    let calories = document.createElement('li');
+    calories.textContent = "calories: " + food.nf_calories;
+    list.appendChild(calories);
+
+    let fat = document.createElement('li');
+    fat.textContent = "fat: " + food.nf_total_fat;
+    list.appendChild(fat);
+
+    let saturatedFat = document.createElement('li');
+    saturatedFat.textContent = "of which saturated fat: " + food.nf_saturated_fat;
+    list.appendChild(saturatedFat);
+
+    let carbohydrates = document.createElement('li');
+    carbohydrates.textContent = "carbohydrates: " + food.nf_total_carbohydrate;
+    list.appendChild(carbohydrates);
+
+    let sugars = document.createElement('li');
+    sugars.textContent = "of which sugars: " + food.nf_sugars;
+    list.appendChild(sugars);
+
+    let fibre = document.createElement('li');
+    fibre.textContent = "fibre: " + food.nf_dietary_fiber;
+    list.appendChild(fibre);
+
+    let proteins = document.createElement('li');
+    proteins.textContent = "proteins: " + food.nf_protein;
+    list.appendChild(proteins);
+
+
+    nutritionInfo.appendChild(list);
+}
+
+document.getElementById('go-back').addEventListener('click', e => {
+    calorieScreen.classList.remove('show');
+    searchScreen.style.display = 'flex';
+    searchBox.value = "";
+    while (dropdown.firstChild) {
+        dropdown.removeChild(dropdown.firstChild);
+    }
+});
 
